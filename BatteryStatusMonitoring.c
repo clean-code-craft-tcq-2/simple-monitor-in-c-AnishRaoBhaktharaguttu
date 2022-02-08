e3456297c135f4dd29ae2fd6b10b84c6d6a7ec5c
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-08 15:41:31 +0530

    Update BatteryStatusMonitoring.c

--- BatteryStatusMonitoring.c
+++ BatteryStatusMonitoring.c
@@ -24,29 +24,31 @@ int printStatusOnConsole(const char* statement) {
 	return 0;
 }
 
-void checkAndPrintStatus(int status, const char* batteryParameter) {
+int checkAndPrintStatus(int status, const char* batteryParameter) {
 	if (status == 0) {
 		char warningMessage[50];
 		strcpy(warningMessage, errorStatus);
 		strcat(warningMessage, batteryParameter);
 		printStatusOnConsole(batteryParameter);
+	} else {
+	   printStatusOnConsole(successStatus);	
 	}
-	printStatusOnConsole(successStatus);	
+	return status;
 }
 
 int checkBatteryTemperature(float temp) {
 	int status = checkIfParameterWithinSpecifiedRange(temp, LOW_THRESHOLD_BATT_TEMP, UPP_THRESHOLD_BATT_TEMP);
-	return checkStatusOfBattery(status, "Battery Temperature");
+	return checkAndPrintStatus(status, "Battery Temperature");
 }
 
 int checkBatteryStateOfCharge(float stateOfCharge) {
 	int status = checkIfParameterWithinSpecifiedRange(stateOfCharge, LOW_THRESHOLD_BATT_SOC, UPP_THRESHOLD_BATT_SOC);
-	return checkStatusOfBattery(status, "Battery State of Charge");
+	return checkAndPrintStatus(status, "Battery State of Charge");
 }
 
 int checkBatteryChargeRate(float chargeRate){
 	int status = checkIfParameterWithinLimit(chargeRate, UPP_THRESHOLD_BATT_CHARGE_RATE); 
-	return checkStatusOfBattery(status, "Battery Charge Rate");
+	return checkAndPrintStatus(status, "Battery Charge Rate");
 }
 
 int checkBatteryCondition(float stateOfCharge, float temp, float chargeRate){
