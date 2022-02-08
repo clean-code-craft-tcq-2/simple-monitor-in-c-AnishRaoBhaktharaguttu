b40e0b25afc6ddf32c6e0bedc00613e16776eb15
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-08 13:30:44 +0530

    Create TestBatteryStatusMonitoring.c

---
+++ TestBatteryStatusMonitoring.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <assert.h>
+#include "BatteryStatusMonitoring.h"
+
+void testCheckBatteryTemperature(float temp, int expStatus) {
+	assert(checkBatteryTemperature(temperature) == expStatus);
+}
+
+void testCheckBatteryChargeRate(float chargeRate, int expStatus){
+	assert(checkBatteryChargeRate(chargeRate) == expStatus);
+}
+ 
+void testCheckBatteryStateOfCharge(float stateOfCharge, int expStatus) {
+	assert(checkBatteryStateOfCharge(stateOfCharge) == expStatus);
+}
+
+void testCheckBatteryCondition(float stateOfCharge, float temp, float chargeRate, int expStatus){
+	assert(checkBatteryCondition(stateOfCharge, temp, chargeRate) == expStatus);
+}
+
+int main() {
+ testCheckBatteryTemperature(-25, 1);
+ testCheckBatteryTemperature(50, 1);
+ testCheckBatteryTemperature(25, 1);
+}
