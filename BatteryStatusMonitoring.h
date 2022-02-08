e14e5243fc0cfdbcbf0ed4c93d779f3271951ced
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-08 13:38:29 +0530

    Update BatteryStatusMonitoring.h

--- BatteryStatusMonitoring.h
+++ BatteryStatusMonitoring.h
@@ -10,4 +10,4 @@ extern int checkBatteryTemperature(float temp);
 extern int checkBatteryStateOfCharge(float stateOfCharge);
 extern int checkBatteryChargeRate(float chargeRate);
 extern int checkBatteryCondition(float stateOfCharge, float temperature, float chargeRate);
-extern int printWarningMessage (const char*);
+extern int printWarningMessage (char*);
