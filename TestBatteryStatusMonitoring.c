5417341565eea303f33fc1cbd835d1ca7346378c
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-15 23:51:34 +0530

    Update TestBatteryStatusMonitoring.c

--- TestBatteryStatusMonitoring.c
+++ TestBatteryStatusMonitoring.c
@@ -9,7 +9,7 @@ void testCheckBatteryCondition(float stateOfCharge, float temp, float chargeRate
 
 int main() {
  testCheckBatteryCondition(60, 30, 0.72, English, 1);
- testCheckBatteryCondition(90, 35, 0.72, German, 0);
+ //testCheckBatteryCondition(90, 35, 0.72, German, 0);
  //testCheckBatteryCondition(60, 53, 0.85, English, 0);
  //testCheckBatteryCondition(60, 35, 0.78, English, 0);
  //testCheckBatteryCondition(60, 35, 0.52, German, 0);
