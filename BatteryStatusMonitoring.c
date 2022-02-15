6fd10483cfb6462ac317a3b0f2e61e58a8b1422e
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-15 20:47:39 +0530

    Update BatteryStatusMonitoring.c

--- BatteryStatusMonitoring.c
+++ BatteryStatusMonitoring.c
@@ -2,19 +2,62 @@
 #include <string.h>
 #include "BatteryStatusMonitoring.h"
 
-char errorStatus[50] = "Out of specified range/limit: ";
-char yellowStatus[50] = "Battery is in low risk";
-char redStatus[50] = "Battery is in high risk";
+// Enum which contains the various states of the parameter
+enum StatusOfParameter = {WITHIN_OPERATING_RANGE = 0,
+			  LOW_LIMIT = 1,
+			  UPPER_LIMIT = 2,
+			  LOW_WARNING_LIMIT = 3,
+			  UPPER_WARNING_LIMIT = 4
+			  };
 
-int checkIfParameterWithinSpecifiedRange(float parameterVal, float lowerThreshold, float upperThreshold) {
-	if (parameterVal < lowerThreshold || parameterVal > upperThreshold) {
+const char* warningMessages[] = {"Battery condition normal and within operating range",
+				 "Battery at Risk: Lower limit exceeded for ",
+				 "Battery at Risk: Upper limit exceeded for ",
+				 "Battery at Risk: Approaching the lower warning limit for ",
+				 "Battery at Risk: Approaching the upper warning limit for "
+				 }
+
+ParameterOperatingStructure tempOperatingLimits;
+
+void setWarningMessage(int StatusOfParameter statusOfParameter, char* warnMsg) {
+	strcpy(warnMsg, warningMessages[statusOfParameter]);
+	strcat(warnMsg,  parameter);
+}
+
+void warnBatteryCondition(const char* parameter, int StatusOfParameter statusOfParameter) {
+	char warnMsg[100];
+	setWarningMessage(parameter, statusOfParameter, warnMsg);	
+	printStatusOnConsole(warnMsg);
+}
+
+int checkIfParameterInWarningZone(ParameterOperatingStructure parameterOperatingStructure){
+         if(parameterOperatingStructure.ParameterValue > parameterOperatingStructure.LowerLimit && 
+	    parameterOperatingStructure.ParameterValue < parameterOperatingStructure.LowerWarningLimit){
+	       warnBatteryCondition(parameterOperatingStructure.ParameterName, LOW_WARNING_LIMIT);
+	 } else if(
+}
+
+int checkIfParameterWithinToleranceRange(ParameterOperatingStructure parameterOperatingStructure, int parameterStateChecked) {
+	if (parameterOperatingStructure.ParameterValue > parameterOperatingStructure.LowerWarningLimit 
+	    && parameterOperatingStructure.ParameterValue < parameterOperatingStructure.UpperWarningLimit){
+	         warnBatteryCondition(parameterOperatingStructure.ParameterName, parameterStateChecked);
+	}else{
+	    checkIfParameterInWarningZone(parameterOperatingStructure);
+	}
+	return 0;
+}
+
+int checkIfParameterWithinLowerLimit(ParameterOperatingStructure paramOperatingStructure, int statusOfParameterInCheck) {
+	if (paramOperatingStructure.ParameterVal  > paramOperatingStructure.LowerLimit) {
+		warnBatteryCondition(parameterOperatingStructure.ParameterName, statusOfParameterInCheck);
 		return 0;
 	}
 	return 1;
 }
 
-int checkIfParameterWithinLimit(float parameterVal, float upperLimit) {
-	if (parameterVal  > upperLimit) {
+int checkIfParameterWithinUpperLimit(ParameterOperatingStructure paramOperatingStructure, int statusOfParameterInCheck) {
+	if (paramOperatingStructure.ParameterVal  > paramOperatingStructure.UpperLimit) {
+		warnBatteryCondition(parameterOperatingStructure.ParameterName, statusOfParameterInCheck);
 		return 0;
 	}
 	return 1;
@@ -25,43 +68,33 @@ int printStatusOnConsole(const char* statement) {
 	return 0;
 }
 
-int checkAndPrintStatus(int status, const char* batteryParameter) {
-	if (status == 0) {
-		char warningMessage[50];
-		strcpy(warningMessage, errorStatus);
-		strcat(warningMessage, batteryParameter);
-		printStatusOnConsole(batteryParameter);
-	} 
-	return status;
-}
-
-void checkAndPrintBatteryCondition(int riskCounter) {
-        if (riskCounter >= RISK_LEVEL){
-		printStatusOnConsole(yellowStatus);
-	}else{
-		printStatusOnConsole(redStatus);
+int checkStatusOfParameter(ParameterOperatingStructure paramOperatingStructure) {
+	int parameterStatus;
+	parameterStatus = checkIfParameterWithinUpperLimit(paramOperatingStructure, UPPER_LIMIT) &&
+		checkIfParameterWithinLowerLimit(paramOperatingStructure, LOW_LIMIT);
+        
+	if(paramOperatingStructure.WarningCheck==1) {
+		parameterStatus = parameterStatus && checkIfParameterWithinToleranceRange(paramOperatingStructure.ParameterValue, WITHIN_OPERATING_RANGE)
 	}
+	
+
+	return parameterStatus;
 }
 
-int checkBatteryTemperature(float temp) {
-	int status = checkIfParameterWithinSpecifiedRange(temp, LOW_THRESHOLD_BATT_TEMP, UPP_THRESHOLD_BATT_TEMP);
-	return checkAndPrintStatus(status, "Battery Temperature");
+void setupParameterOperatingStructure(ParameterOperatingStructure paramOperatingStructure, float parameterValue, float lowerLimit, float upperLimit,
+				      float lowWarningLimit, float upperWarningLimit, const char* parameter, int warningCheck){
+       paramOperatingStructure.ParameterValue = temperatureValue;
+       paramOperatingStructure.LowerLimit = lowerLimit;
+       paramOperatingStructure.UpperLimit = upperLimit;
+       paramOperatingStructure.LowerWarningLimit = lowWarningLimit;
+       paramOperatingStructure.UpperWarningLimit = upperWarningLimit;
+       paramOperatingStructure.ParameterName = parameter;
+       paramOperatingStructure.WarningCheck = warningCheck;
 }
-
-int checkBatteryStateOfCharge(float stateOfCharge) {
-	int status = checkIfParameterWithinSpecifiedRange(stateOfCharge, LOW_THRESHOLD_BATT_SOC, UPP_THRESHOLD_BATT_SOC);
-	return checkAndPrintStatus(status, "Battery State of Charge");
-}
-
-int checkBatteryChargeRate(float chargeRate){
-	int status = checkIfParameterWithinLimit(chargeRate, UPP_THRESHOLD_BATT_CHARGE_RATE); 
-	return checkAndPrintStatus(status, "Battery Charge Rate");
-}
-
 
 int checkBatteryCondition(float stateOfCharge, float temp, float chargeRate){
-	int riskCounter = 0;
-	riskCounter = (checkBatteryTemperature(temp)) + (checkBatteryStateOfCharge(stateOfCharge)) + (checkBatteryChargeRate(chargeRate));
-	checkAndPrintBatteryCondition(riskCounter);
-	return riskCounter;	
+	setupParameterOperatingStructure(tempOperatingLimits, temp, LOW_THRESHOLD_BATT_TEMP, UPP_THRESHOLD_BATT_TEMP, 
+					 LOW_TOLERANCE_BATT_TEMP, HIGH_TOLERANCE_BATT_TEMP, "Temperature" TEMP_WARNING_CHECK);
+	int batteryStatus =  (checkStatusOfParameter(tempOperatingLimits));
+	return batteryStatus;	
 }
