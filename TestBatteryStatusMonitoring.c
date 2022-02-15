a826228e8a564354455723c90ff2579ba831a819
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-16 00:33:15 +0530

    Update TestBatteryStatusMonitoring.c

--- TestBatteryStatusMonitoring.c
+++ TestBatteryStatusMonitoring.c
@@ -11,4 +11,11 @@ int main() {
 	testCheckBatteryCondition(45, 20, 0.6, English, 1);
 	testCheckBatteryCondition(90, 43, 0.6, English, 0);
 	testCheckBatteryCondition(92, 20, 0.63, German, 0);
+	testCheckBatteryCondition(2, 72, 0.9, German, 0);
+	testCheckBatteryCondition(21, 90, 0.63, English, 0);
+	testCheckBatteryCondition(23, 78, 0.67, English, 0);
+	testCheckBatteryCondition(21, 22, 0.59, English, 0);
+	testCheckBatteryCondition(22, 10, 0.52, German, 0);
+	testCheckBatteryCondition(21, 45, 0.91, English, 0);
+	testCheckBatteryCondition(25, 45, 0.01, German, 0);
 }
