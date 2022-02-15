53d400c3cf16c5bc4af7bc62684b8941e9a58286
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-15 23:40:55 +0530

    Update TestBatteryStatusMonitoring.c

--- TestBatteryStatusMonitoring.c
+++ TestBatteryStatusMonitoring.c
@@ -3,10 +3,10 @@
 #include "BatteryStatusMonitoring.h"
 
 
-void testCheckBatteryCondition(float stateOfCharge, float temp, float chargeRate, int expStatus){
-	assert(checkBatteryCondition(stateOfCharge, temp, chargeRate) == expStatus);
+void testCheckBatteryCondition(float stateOfCharge, float temp, float chargeRate, enum chosenLanguage language, int expStatus){
+	assert(checkBatteryCondition(stateOfCharge, temp, chargeRate, language) == expStatus);
 }
 
 int main() {
- testCheckBatteryCondition(60, 30, 0.72, 1);
+ testCheckBatteryCondition(60, 30, 0.72, English, 1);
 }
